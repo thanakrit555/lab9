e5f144713ef1a70c7b13c52ea1a3df43fcd59bc1
thanakrit555 <59599161+thanakrit555@users.noreply.github.com>
2021-01-08 16:21:10 +0700

    Update lab9_3.cpp

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,26 +1,25 @@
-#include<iostream>
-#include<iomanip>
-#include<fstream>
+#include <iostream>
+#include <fstream>
+#include <string>
 #include<cstdlib>
 #include<cmath>
 using namespace std;
+
 int main(){
-    ifstream source("score.txt");
-    int i =0;
-    double average = 0, SD,sum = 0;
-    string textline;
-    while (getline(source,textline))
-    {
-        average += atof(textline.c_str());
-        sum += pow((atof(textline.c_str())),2);
-        i++;
-    }
-    average = average/i;
-    SD = sqrt((sum/i)-pow(average,2));
-    cout << "Number of data = "<<i<<endl;
-    cout << setprecision(3);
-    cout << "Mean = " << average << endl;
-    cout << "Standard deviation = " << SD << endl;
-    source.close();
-    return 0;
+	
+	int count =0;
+	float sum =0;
+	float y =0;
+	string textline;
+	ifstream source("C:\\Users\\LaB_314\\Desktop\\score.txt"); 
+	while (getline(source,textline)){
+		cout<<textline<<"\n";
+		sum += atof(textline.c_str());
+		y+=pow(atof(textline.c_str()),2);
+		count ++;
+	}
+	cout << "Number of data = "<<count<<"\n";
+    cout << "Mean = "<<sum/count<<"\n";
+    cout << "Standard deviation = "<<sqrt(y/count-pow(sum/count,2)) <<"\n";
 }
+
