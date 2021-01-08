4969750d790c9e5f044a86b3123be4e1d4b87646
thanakrit555 <59599161+thanakrit555@users.noreply.github.com>
2021-01-08 15:59:39 +0700

    lab9_3 completed

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,39 @@
 //cout << "Number of data = ";
 //cout << "Mean = ";
 //cout << "Standard deviation = ";
+
+/*#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<cstdlib>
+#include<cmath>
+using namespace std;
+int main(){
+    ifstream source("score.txt");
+    int i =0;
+    double average = 0, SD,sum = 0;
+    string textline;
+    while (getline(source,textline))
+    {
+        average += atof(textline.c_str());
+        sum += pow((atof(textline.c_str())),2);
+        i++;
+    }
+    average = average/i;
+    SD = sqrt((sum/i)-pow(average,2));
+    cout << "Number of data = "<<i<<endl;
+    cout << setprecision(3);
+    cout << "Mean = " << average << endl;
+    cout << "Standard deviation = " << SD << endl;
+    source.close();
+    return 0;
+}*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i =2;i<=6;i++){
+        cout<<"C";
+    }
+    return 0;
+}
